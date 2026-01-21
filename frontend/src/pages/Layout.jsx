947cdba3b2ce2071c71effdd01cbacef7698e99d
74sonuydv <yadavsonu34785@gmail.com>
2026-01-21 22:35:43 +0530

    add login and sidebar

---
+++ frontend/src/pages/Layout.jsx
@@ -0,0 +1,33 @@
+import { Heading1, Menu, X } from 'lucide-react';
+import Sidebar from '../components/Sidebar'
+import React, { useState } from 'react'
+import { Outlet } from 'react-router-dom'
+import { dummyUserData } from '../assets/assets';
+import Loading from '../components/Loading';
+
+const Layout = () => {
+
+  const [sidebarOpen, setSidebarOpen] = useState(false);
+  const user = dummyUserData;
+
+  return user ? (
+    <div className='w-full flex h-screen relative'>
+
+      <Sidebar sidebarOpen={sidebarOpen} setSidebarOpen={setSidebarOpen}/>
+
+      <div className='flex-1 bg-slate-50'>
+        <Outlet />
+      </div>
+      {
+        sidebarOpen ?
+          <X className='absolute top-3 right-3 p-2 z-100 bg-white rounded-md shadow w-10
+         h-10 text-gray-600 sm:hidden' onClick={() => setSidebarOpen(false)}/>
+          :
+          < Menu className='absolute top-3 right-3 p-2 z-100 bg-white rounded-md shadow w-10
+         h-10 text-gray-600 sm:hidden ' onClick={() => setSidebarOpen(true)}/>
+      }
+    </div>
+  ) : (<Loading/>)
+} 
+
+export default Layout
